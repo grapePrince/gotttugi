8d78bfd2b249816ff3283e1db86c589544f1230a
yeji.lee <tul90@naver.com>
2019-07-11 18:10:41 +0900

    [코딩] 헤더 거의 마무리 되어가는거 같은데...

--- site/js/main.js
+++ site/js/main.js
@@ -1,11 +1,53 @@
 $(function() {
+    var $menuList = $('nav > ul > li');
+    var $nav = $('nav');
+    var $searchBtn = $('header .upper_nav .search');
+    var $header = $('header');
 
     $('.sliderWrap').bxSlider();
-    $('.dropdown_menu').on('click', function() {
+
+    addEventListener();
+
+
+    function addEventListener() {
+        $('.dropdown_menu').on('click', dropDownMenuClicked);
+        $('.dropdown_menu').on('mouseleave', dropDownMenuMouseLeaved);
+
+        $nav.on('mouseenter', navMouseEntered)
+            .on('mouseleave', navMouseLeaved);
+
+        $menuList.on('mouseenter', gnbListMouseEntered)
+                 .on('mouseleave', gnbListMouseLeaved); 
+        $searchBtn.on('click', searchBtnClicked);            
+    }
+
+    function dropDownMenuClicked() {
         $(this).toggleClass('active');
-    });
-    $('.dropdown_menu').on('mouseleave', function() {
+    }
+
+    function dropDownMenuMouseLeaved() {
         $(this).removeClass('active');
-    });
+    }
+
+    function navMouseEntered() {
+        $header.addClass('open_sub');
+    }
+
+    function navMouseLeaved() {
+        $header.removeClass('open_sub');
+    }
+
+    function gnbListMouseEntered() {
+        var className = $(this).attr('data-class');
+        $header.addClass(className);
+    }
+
+    function gnbListMouseLeaved() {
+        $header.removeClass('corporation products recipe media');
+    }
+
+    function searchBtnClicked() {
+        
+    }
 
 }); // end of file
