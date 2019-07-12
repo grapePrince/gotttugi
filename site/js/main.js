2b63e8d16df429f45a89b385d367a85fe1a08881
yeji.lee <tul90@naver.com>
2019-07-12 17:57:18 +0900

    [개발] 모바일쪽 메뉴 동작 완료

--- site/js/main.js
+++ site/js/main.js
@@ -1,48 +1,118 @@
 $(function() {
-    var $menuList = $('nav > ul > li');
-    var $nav = $('nav');
-    var $searchBtn = $('header .upper_nav .search');
+    var $window = $(window);    
     var $header = $('header');
-    var $navCloseBtn = $('nav .background .close');
+    var $gnbMenuList = $('header nav > ul > li');
+    var $menuBtn = $('header .upper_nav .menu');
+
+    var isMobile = false;
+    var CLASS_SEARCH_OPENED = 'open_search';
+    var CLASS_SUB_OPENED = 'open_sub';
+    var CLASS_SETTING_OPENED = 'open_setting';
+    var CLASS_ACTIVE = 'active';
 
     $('.sliderWrap').bxSlider();
 
     addEventListener();
+    isMobile = ($(window).outerWidth() <= 600) ? true : false;
 
     function addEventListener() {
-        $('.dropdown_menu').on('click', dropDownMenuClicked);
-        $('.dropdown_menu').on('mouseleave', dropDownMenuMouseLeaved);
+        $window.on('resize', resizeHandler);
 
-        $nav.on('mouseenter', navMouseEntered)
-            .on('mouseleave', navMouseLeaved);
+        $('nav')
+        .on('mouseenter', navMouseEntered)
+        .on('mouseleave', navMouseLeaved);
 
-        $menuList.on('mouseenter', gnbListMouseEntered)
-                 .on('mouseleave', gnbListMouseLeaved); 
+        $('nav > ul > li')
+        .on('mouseenter', gnbListMouseEntered)
+        .on('mouseleave', gnbListMouseLeaved); 
 
-        $searchBtn.on('click', searchBtnClicked);  
-        $navCloseBtn.on('click', navCloseBtnClicked);          
+        addClassRelated();         
     }
 
-    function dropDownMenuClicked() {
-        $(this).toggleClass('active');
+    function addClassRelated() {
+        $('.dropdown_menu').on('click', handleClass);
+        $('.dropdown_menu').on('mouseleave', {
+            func: 'remove'
+        }, handleClass);
+        $('header .upper_nav .search').on('click', {
+            className: CLASS_SEARCH_OPENED,
+            target: $header
+        }, handleClass);  
+
+        $('header .upper_nav .option').on('click', {
+            className: CLASS_SETTING_OPENED,
+            target: $header
+        }, handleClass);
+
+        $('nav .background .close').on('click', {
+            className: CLASS_SEARCH_OPENED,
+            target: $header,
+            func: 'remove'
+        }, handleClass);   
+
+        $menuBtn.on('click', {         // 메뉴 버튼이 눌렸을 때 active 되어있던 li는 초기화해준다. 
+            target: $gnbMenuList,
+            func: 'remove'
+        }, handleClass); 
+        $menuBtn.on('click', {
+            className: CLASS_SUB_OPENED, 
+            target: $header
+        }, handleClass); 
+
+        $gnbMenuList.on('click', handleClass);          
     }
 
-    function dropDownMenuMouseLeaved() {
-        $(this).removeClass('active');
+    /*
+     * 클릭했을 때 특정 클래스 넣고 빼고만 하는 경우
+     * className: 클래스이름. 디폴트는 active
+     * target: 어디에 동작할 것인지. 없으면 this 사용.
+     * func: 어떤 함수를 사용할 것인지. remove 인경우 removeClass, 디폴트는 toggleClass
+     */
+
+    function handleClass(e) {
+        e.preventDefault();
+
+        var target = $(this);
+        var className = CLASS_ACTIVE;
+        var func = 'toggle';
+        if (e.data) {
+            target = e.data.target ? e.data.target : $(this);
+            className = e.data.className ? e.data.className : CLASS_ACTIVE;
+            func = e.data.func ? e.data.func : 'toggle';
+        }
+
+        if (func === 'remove') {
+            target.removeClass(className);  
+        } else {
+            target.toggleClass(className);   
+        }            
+    }
+
+    function resizeHandler(e) {
+        if ($(window).outerWidth() > 600 && isMobile) { // pc
+            isMobile = false;
+            // 값이 바뀔 때 한 번만 header 안의 class를 정리해준다. 
+            $header.removeClass(CLASS_SEARCH_OPENED + ' ' + CLASS_SUB_OPENED + ' ' + CLASS_SETTING_OPENED);
+        } else if($(window).outerWidth() <= 600 && isMobile) { // mobile
+            isMobile = true;
+            $header.removeClass(CLASS_SEARCH_OPENED + ' ' + CLASS_SUB_OPENED + ' ' + CLASS_SETTING_OPENED);
+        }
     }
 
     function navMouseEntered() {
-        if (!$header.hasClass('open_search')) { // 검색창을 열어놓은 경우에는 서브메뉴가 열리지 않는다. 
-            $header.addClass('open_sub');
+        if (!$header.hasClass(CLASS_SEARCH_OPENED)) { // 검색창을 열어놓은 경우에는 서브메뉴가 열리지 않는다. 
+            $header.addClass(CLASS_SUB_OPENED);
         }
     }
 
     function navMouseLeaved() {
-        $header.removeClass('open_sub');
+        if (!isMobile) {
+            $header.removeClass(CLASS_SUB_OPENED);
+        }
     }
 
     function gnbListMouseEntered() {
-        if ($header.hasClass('open_sub')) { // 서브메뉴가 열려있는 동안만 이미지를 보여준다. 
+        if ($header.hasClass(CLASS_SUB_OPENED) && !isMobile) { // 서브메뉴가 열려있는 동안만 이미지를 보여준다. 
             var className = $(this).attr('data-class');
             var $img = $('header nav .background img').filter('.' + className);
             $img.stop().delay(200).fadeIn();
@@ -55,12 +125,8 @@ $(function() {
         $img.stop().hide();
     }
 
-    function searchBtnClicked() {
-        $header.toggleClass('open_search');
-    }
-
-    function navCloseBtnClicked() {
-        $header.removeClass('open_search');
+    function menuBtnClicked() { 
+        $gnbMenuList.removeClass('active');
     }
 
 }); // end of file
