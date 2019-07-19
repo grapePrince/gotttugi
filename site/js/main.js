589b7a6607af4ee2e6971e97579ffafe077f8480
yeji.lee <tul90@naver.com>
2019-07-19 18:06:38 +0900

    [코딩] 스와이퍼로 바꿔볼 거시다.

--- site/js/main.js
+++ site/js/main.js
@@ -5,18 +5,22 @@ $(document).ready(function () {
   var $menuBtn = $("header .upper_nav .menu");
 
   var isMobile = $(window).outerWidth() <= 600 ? true : false;
+  var underSliderWidth = $(window).outerWidth() <= 1100 ? true : false;
   var corporationSlideNum = 0;
 
   var CLASS_SEARCH_OPENED = "open_search";
   var CLASS_SUB_OPENED = "open_sub";
   var CLASS_SETTING_OPENED = "open_setting";
   var CLASS_ACTIVE = "active";
+  var FULLPAGE_WIDTH = 900;
+  var NEWS_SLIDER_WIDTH = 1110;
 
   var bannerSlider;
   var newsSlider;
   var fullPage;
+  var firstRebuild = false;
 
-  if (isMobileDevice()) { // 모바일 디바이스인 경우에는 fullpage.js 를 로드하지 않는다. 
+  if (isMobileDevice()) { // 모바일 디바이스인 경우에는 fullpage.js 를 아예 로드하지 않는다. 
     if (!bannerSlider) {
       bannerSlider = $(".sliderWrap").bxSlider({
         pager: false,
@@ -24,25 +28,7 @@ $(document).ready(function () {
         nextSelector: ".banner .right",
         onSliderLoad: function () {
           if (!newsSlider) {
-            newsSlider = $(".slider__wrapper--news").bxSlider({
-              pager: true,
-              controls: false,
-              pagerSelector: ".slider__controls--news",
-              minSlides: 1,
-              maxSlides: 3,
-              moveSlides: 1,
-              slideWidth: 600,
-              slideMargin: 50,
-              auto: true,
-              pause: 3000,
-              scrollBar: true,
-              onSliderLoad: function () {
-                $('video').each(function () {
-                  $(this).get(0).play();
-                });
-                addEventListener();
-              }
-            });
+            loadNewsSlider();        
           }
         }
       });
@@ -50,7 +36,7 @@ $(document).ready(function () {
   } else {
     fullPage = new fullpage("#fullpage", {
       //options here
-      responsiveWidth: 900,
+      responsiveWidth: FULLPAGE_WIDTH,
       scrollOverflow: true,
       afterRender: function () {
         if (!bannerSlider) {
@@ -60,46 +46,75 @@ $(document).ready(function () {
             nextSelector: ".banner .right",
             onSliderLoad: function () {
               if (!newsSlider) {
-                newsSlider = $(".slider__wrapper--news").bxSlider({
-                  pager: true,
-                  controls: false,
-                  pagerSelector: ".slider__controls--news",
-                  minSlides: 1,
-                  maxSlides: 3,
-                  moveSlides: 1,
-                  slideWidth: 600,
-                  slideMargin: 50,
-                  auto: true,
-                  pause: 3000,
-                  onSliderLoad: function () {
-                    addEventListener();
-                    // bxSlider 가 로드되어 높이가 정리된 후엔 rebuild 하여 높이 재정리
-                    // setTimeout 사용하지 않을시 IE 에서 rebuild 함수 호출 오류 
-                    // 정의되지 않음 또는 null 참조인 'createScrollBarForAll' 속성을 가져올 수 없습니다. 확인 필요  
-                    setTimeout(function () {
-                      fullpage_api.reBuild();
-                    }, 100);                      
-                  }
-                });
+                loadNewsSlider();                
               }
             }
           });
         }
       },
       afterReBuild: function () {
-        if ( $('.fp-responsive').length < 1) {
+        if (
+          firstRebuild 
+          ||  
+          ($(window).outerWidth() > FULLPAGE_WIDTH && $('.fp-scrollable').length < 1)
+          ) {
           // fullPage.js 에서 scrollOverflow:true , fp-auto-height-responsive 를 함께 썼을 때
           // window resize를 통해 모바일 구간에서 pc 구간으로 이동하는 경우 
           // 처음 한번은 스크롤 엘리먼트가 제대로 들어오지 않는 이슈 대응.
+          console.log('rebuild');
           setTimeout(function () {
             $(window).resize();
           }, 500);
+          firstRebuild = false;
         }
       }
     });
   }
 
+  function loadNewsSlider() {
+    var config = {
+      pager: true,
+      controls: false,
+      pagerSelector: ".slider__controls--news",
+      minSlides: 3,
+      maxSlides: 3,
+      moveSlides: 1,
+      slideWidth: 600,
+      slideMargin: 500,
+      auto: true,
+      pause: 10000,
+      scrollBar: true,
+      infiniteLoop: false,
+      onSliderLoad: function () {
+        if (isMobile) {
+          $('video').each(function () {
+            $(this).get(0).play();
+          });
+        }
+        addEventListener();          
+        if (!isMobileDevice()) { // 모바일 디바이스인 경우 풀페이지를 로드하지 않는다. 
+        // bxSlider 가 로드되어 높이가 정리된 후엔 rebuild 하여 높이 재정리
+        // setTimeout 사용하지 않을시 IE 에서 rebuild 함수 호출 오류 
+        // 정의되지 않음 또는 null 참조인 'createScrollBarForAll' 속성을 가져올 수 없습니다. 확인 필요  
+          setTimeout(function () {
+            firstRebuild = true;
+            fullpage_api.reBuild();
+          }, 100);   
+        }
+      }
+    }
 
+    if ($(window).outerWidth() > NEWS_SLIDER_WIDTH) {
+      config.maxSlides = 3;
+    } else {
+      config.maxSlides = 1;
+    }
+    if(newsSlider) {
+      newsSlider.reloadSlider(config);
+    } else {      
+      newsSlider = $(".slider__wrapper--news").bxSlider(config);
+    }
+  }
 
   function addEventListener() {
     $window.on("resize", resizeHandler);
@@ -223,7 +238,19 @@ $(document).ready(function () {
         CLASS_SETTING_OPENED
       );
     }
-    fullpage_api.reBuild(); // 화면 높이, 너비 등등이 바뀌고 나면 풀페이지 라이브러리 재로드. 
+
+    // NEWS_SLIDER_WIDTH 경계로 슬라이더 옵션을 변경해서 재로드한다. 
+    if (width > NEWS_SLIDER_WIDTH && underSliderWidth) {
+      underSliderWidth = false;
+      loadNewsSlider();
+    } else if (width <= NEWS_SLIDER_WIDTH && !underSliderWidth) {
+      underSliderWidth = true;
+      loadNewsSlider();
+    } 
+
+    if(fullpage_api) {
+      fullpage_api.reBuild(); // 화면 높이, 너비 등등이 바뀌고 나면 풀페이지 라이브러리 재로드. 
+    }
   }
 
   function navMouseEntered() {
